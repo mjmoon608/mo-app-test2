4c16c08501da22d5b7c4f2ccb5dfefad6067c6ec
DP_Bell <tpqms608@naver.com>
2020-05-18 21:21:55 +0900

    edit tab navigation
    tab bar icon 추가,
    선택시 색상 메인컬러로 변경

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Main from "../screens/Main";
 import Restaurant from "../screens/Restaurant";
 import Favorite from "../screens/Favorite";
 import More from "../screens/More";
+import MAINCOLOR from "../color/MainColor";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -23,7 +26,33 @@ export default ({ navigation, route }) => {
   //     });
   //   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Main") {
+            iconName += "home";
+          } else if (route.name === "Restaurant") {
+            iconName += "restaurant";
+          } else if (route.name === "Favorite") {
+            iconName += "heart";
+          } else if (route.name === "More") {
+            iconName = "ios-more";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? MAINCOLOR : "grey"}
+              size={26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: true, // tabbar에 라벨 지우고 싶을 때 true 로 변경.
+        activeTintColor: MAINCOLOR,
+      }}
+    >
       <Tabs.Screen name="Main" component={Main} />
       <Tabs.Screen name="Restaurant" component={Restaurant} />
       <Tabs.Screen name="Favorite" component={Favorite} />
